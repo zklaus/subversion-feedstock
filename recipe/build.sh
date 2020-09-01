53ff3d210c457bbf8fd1f25b49e5d775a9ddc36c
Ken Odegard <ken.odegard@gmail.com>
2020-09-01 10:06:18 -0500

    Updated build and tests

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,11 +10,11 @@ export CFLAGS="${CFLAGS} -U__USE_XOPEN2K -std=c99"
   --with-sqlite="${PREFIX}" \
   --disable-static \
   --with-sysroot="${CONDA_BUILD_SYSROOT}"
+
 make -j ${CPU_COUNT}
 make -j ${CPU_COUNT} check CLEANUP=true TESTS=subversion/tests/cmdline/basic_tests.py
 make install
 
-make swig-pl-lib
-make install-swig-pl-lib
-cd subversion/bindings/swig/perl/native/
-perl Makefile.PL INSTALL_BASE="${PREFIX}"
+make swig-pl
+make check-swig-pl
+make install-swig-pl
