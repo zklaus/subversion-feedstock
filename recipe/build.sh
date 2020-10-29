8b05b8109ea11d7367f96b69ee57e12f94f65591
Klaus Zimmermann <klaus.zimmermann@smhi.se>
2020-10-29 13:49:26 +0100

    Try other method of passing sysroot

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 set -ex
 
-export CFLAGS="${CFLAGS} -U__USE_XOPEN2K -std=c99"
+if [[ -v MACOSX_DEPLOYMENT_TARGET ]] ; then
+    export CFLAGS="-isysroot ${CONDA_BUILD_SYSROOT} -mmacosx-version-min=${MACOSX_DEPLOYMENT_TARGET} ${CFLAGS} -U__USE_XOPEN2K -std=c99"
+else
+    export CFLAGS="${CFLAGS} -U__USE_XOPEN2K -std=c99"
+fi
 
 ./configure \
   --prefix="${PREFIX}" \
   --enable-svnxx \
   --enable-bdb6 \
   --with-sqlite="${PREFIX}" \
-  --disable-static \
-  --with-sysroot="${CONDA_BUILD_SYSROOT}"
+  --disable-static
 
 make -j ${CPU_COUNT}
 make -j ${CPU_COUNT} check CLEANUP=true TESTS=subversion/tests/cmdline/basic_tests.py
